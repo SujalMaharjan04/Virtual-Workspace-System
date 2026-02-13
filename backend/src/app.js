c977f7cee4703b24ba6ccf7b5dfda83f4914e0a4
SujalMaharjan04 <maharjan.sujal100@gmail.com>
2026-02-13 13:36:26 +0545

    feature: auth Route in use

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,29 +1,10 @@
-require('dotenv').config()
 const express = require('express')
 const prisma = require("./db")
+const authRouter = require('../routes/authRoutes')
 
 const app = express()
 app.use(express.json())
 
-app.get("/users", async (req, res) => {
-    try {
-        const users = await prisma.user.findMany();
-        res.json(users)
-    } catch (error) {
-        res.status(500).json({error: error.message})
-    }
-})
-
-app.post("/users", async(req, res) => {
-    try {
-        const {name, email, password} = req.body
-        const user = await prisma.user.create({
-            data: { name, email, password }
-        })
-        res.json(user)
-    } catch (error) { 
-        res.status(500).json({error: error.message})
-    }
-})
+app.use('/api/auth', authRouter)
 
 module.exports = app
