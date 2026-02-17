306768c9208ecfa75b08f188033c3d696c68709b
SujalMaharjan04 <maharjan.sujal100@gmail.com>
2026-02-17 11:53:56 +0545

    feature: Added socket io connection

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,7 +1,17 @@
 const app = require("./app")
 const http = require('http')
+const { Server } = require('socket.io')
 
 const server = http.createServer(app)
+const io = new Server(server)
+
+io.on("connection", (socket) => {
+    console.log("you have connected to websocket", socket.id)
+})
+
+app.get("/", (req, res) => {
+    res.send("<h1>Hello</h1>")
+})
 
 server.listen(3001, () => {
     console.log("server running on port 3001")
