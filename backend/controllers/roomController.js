34c2e4d8af458e3681647fb655e0276d4c18a9c5
SujalMaharjan04 <maharjan.sujal100@gmail.com>
2026-02-17 11:54:13 +0545

    feature: New Join room and generate room id functions

--- backend/controllers/roomController.js
+++ backend/controllers/roomController.js
@@ -27,4 +27,24 @@ const createRoom = async(req, res) => {
     }
 }
 
-module.exports = {createRoom}
+const joinRoom = async(req, res) => {
+    try {
+        const {id, password} = req.body
+
+        if (!id || !password) {
+            return res.status(400).json({error: "Id and Password should be given"})
+        }
+
+        const room = await roomService.joinRoom({id, password})
+
+        res.status(200).json(room)
+
+    }
+
+    catch (error) {
+        console.log(error.message)
+        res.status(500).json({error: error.message})
+    }
+}
+
+module.exports = {createRoom, joinRoom}
--- backend/services/room.service.js
+++ backend/services/room.service.js
@@ -1,12 +1,33 @@
+require('dotenv').config()
 const bcrypt = require('bcrypt')
 const prisma = require("../src/db")
+const jwt = require('jsonwebtoken')
+
+const generateRoomId = () => {
+    return Math.floor(100000 + Math.random() * 900000).toString()
+}
 
 const createRoom = async({name, password}) => {
     try {
         const hashedPassword = await bcrypt.hash(password, 10)
 
+        let room_id
+        let isUnique = false
+
+        while (!isUnique) {
+            room_id = generateRoomId()
+            
+            const existing = await prisma.room.findUnique({
+                where: {room_id}
+            })
+
+            if (!existing) {
+                isUnique = true
+            }
+        }
+
         const room = await prisma.room.create({
-            data: {room_name: name, room_password: hashedPassword}
+            data: {room_id, room_name: name, room_password: hashedPassword}
         })
         
         return room
@@ -17,4 +38,27 @@ const createRoom = async({name, password}) => {
     }
 }
 
-module.exports = {createRoom}
+const joinRoom = async({id, password}) => {
+    try {
+        const room = await prisma.room.findUnique({
+            where: {room_id: id}
+        })
+
+        if (!room) throw new Error("No Room with that id found")
+
+        const passwordCheck = await bcrypt.compare(password, room.room_password)
+
+        if (!passwordCheck) throw new Error('Password incorrect')
+
+        const payload = {id, name: room.room_name}
+
+        const token = jwt.sign(payload, process.env.SECRET, {expiresIn: "1D"})
+
+        return token
+    }
+    catch (error) {
+        throw error
+    }
+}
+
+module.exports = {createRoom, joinRoom}
