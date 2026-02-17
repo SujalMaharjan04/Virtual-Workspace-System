ad541b5cc5bdb283029eed10529905ab6ec55457
SujalMaharjan04 <maharjan.sujal100@gmail.com>
2026-02-17 11:54:45 +0545

    feature: initialized the roomRoutes in the app

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,11 +1,13 @@
 const express = require('express')
 
 const authRouter = require('../routes/authRoutes')
+const roomRouter = require('../routes/roomRoutes')
 
 const app = express()
 app.use(express.json())
 
 
 app.use('/api/auth', authRouter)
+app.use("/api/room", roomRouter)
 
 module.exports = app
