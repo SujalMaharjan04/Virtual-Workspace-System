e31f1f787aa6576c567981ace60ef16ebd3fe9a4
SujalMaharjan04 <maharjan.sujal100@gmail.com>
2026-02-21 21:39:15 +0545

    fix: Removed unused feature

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,10 +1,11 @@
 const express = require('express')
-
+const path = require('path')
 const authRouter = require('../routes/authRoutes')
 const roomRouter = require('../routes/roomRoutes')
 
 const app = express()
 app.use(express.json())
+app.use(express.static(path.resolve('./public')))
 
 
 app.use('/api/auth', authRouter)
