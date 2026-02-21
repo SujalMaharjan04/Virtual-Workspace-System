ff242dc8ae9d2fb0a51726da4bbb79fe6cc8e382
SujalMaharjan04 <maharjan.sujal100@gmail.com>
2026-02-21 21:38:08 +0545

    feature: Middleware to extract user via their token

---
+++ backend/utils/middleware.js
@@ -0,0 +1,30 @@
+const jwt = require('jsonwebtoken')
+const config = require("./config")
+
+
+const userExtractor = (req, res, next) => {
+    const authorization = req.get("authorization")
+
+    if (authorization && authorization.includes("Bearer ")) {
+        req.token = authorization.substring(7)
+    } else {
+        req.token = null
+        next()
+    }
+
+    try {
+        const decodedToken = jwt.verify(req.token, config.SECRET)
+        if (!decodedToken.id) {
+            return res.status(401).json({error: "Invalid token"})
+        }
+
+        req.user = decodedToken
+        next()
+    }
+
+    catch (error) {
+        return res.status(400).json(error.message)
+    }
+}
+
+module.exports = {userExtractor}
