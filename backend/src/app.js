cb2d27da54d73ec7d1a9264fdfd8e6a5e63669d1
SujalMaharjan04 <maharjan.sujal100@gmail.com>
2026-02-22 21:43:29 +0545

    fix: Initialized server in app.js itself

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,14 +1,17 @@
 const express = require('express')
 const path = require('path')
+const {initializeServer} = require('../sockets/socket')
+const http = require('http')
 const authRouter = require('../routes/authRoutes')
 const roomRouter = require('../routes/roomRoutes')
 
 const app = express()
+const server = http.createServer(app)
 app.use(express.json())
 app.use(express.static(path.resolve('./public')))
 
-
+initializeServer(server)
 app.use('/api/auth', authRouter)
 app.use("/api/room", roomRouter)
 
-module.exports = app
+module.exports = {app, server}
