949f63d1de1f5d863ca0bd56d78e3f3500791b41
SujalMaharjan04 <maharjan.sujal100@gmail.com>
2026-03-07 14:26:23 +0545

    feature: New test regarding joining room and socket connection initialization

--- backend/test/roomTest.test.js
+++ backend/test/roomTest.test.js
@@ -11,7 +11,6 @@ const http = require("http")
 let server, clientSocket, port
 
 before(async() => {
-    server = http.createServer(app)
     await new Promise((resolve, reject) => {
         appServer.listen(3001, () => {
             port = appServer.address().port
@@ -21,6 +20,10 @@ before(async() => {
 })
 
 beforeEach(async() => {
+    if (clientSocket) {
+        clientSocket.disconnect()
+        clientSocket = null
+    }
     await prisma.room.deleteMany({})
     await prisma.user.deleteMany({})
 
@@ -55,40 +58,142 @@ describe("Testing for the rooms", () => {
             password: "123456"
         }
 
-        await api.post("/api/room/createroom").send(newRoom).set('Authorization', `Bearer ${response.body.token}`).expect(201).expect("Content-type", /application\/json/)
+        const room = await api.post("/api/room/createroom").send(newRoom).set('Authorization', `Bearer ${response.body.token}`).expect(201).expect("Content-type", /application\/json/)
 
         const roomNow = await helper.roomInDb()
         assert.strictEqual(roomNow.length, roomAtStart.length + 1)
-        
+        assert.ok(room.body.room.id)
     })
 })
 
 describe("Testing Socket.io", () => {
-    test("Should connect to socket with valid token", async() => {
-        const loginInfo = {
+    // test("Should connect to socket with valid token", async() => {
+    //     const loginInfo = {
+    //         email: "bob@gmail.com",
+    //         password: "123456"
+    //     }
+
+    //     const response = await api
+    //                         .post("/api/auth/login")
+    //                         .send(loginInfo)
+
+    //     const token = response.body.token
+
+    //     await new Promise((resolve, reject) => {
+    //         clientSocket = new Client(`http://localhost:${port}`, {
+    //             auth: {token}
+    //         })
+
+    //         clientSocket.on("connect", () => {
+    //             assert.strictEqual(clientSocket.connected, true)
+    //             resolve()
+    //         })
+
+    //         clientSocket.on("connect_error", (err) => {
+    //             reject(err)
+    //         })
+    //         })
+    // })
+
+    test('Should join the room', async() => {
+        const loginUser = {
             email: "bob@gmail.com",
             password: "123456"
         }
 
-        const response = await api
-                            .post("/api/auth/login")
-                            .send(loginInfo)
+        const user = await api.post("/api/auth/login").send(loginUser).expect(201).expect("Content-type", /application\/json/)
 
-        const token = response.body.token
+        const newRoom = {
+            name: "Bob's Room",
+            password: "123456"
+        }
 
+        const room = await api.post("/api/room/createroom").send(newRoom).set("Authorization", `Bearer ${user.body.token}`).expect(201).expect("Content-type", /application\/json/)
+
+        const roomId = room.body.room.id
+
+        const joinRoom = {
+            roomId,
+            password: "123456"
+        }
+
+        const joinedRoom = await api.post("/api/room/join").set("Authorization", `Bearer ${user.body.token}`).send(joinRoom).expect(200).expect("Content-type", /application\/json/)
+        
+        assert.ok(joinedRoom.body.token)
+
+    })
+
+    test("after joining the room, socket connection is established", async() => {
+        const loginUser = {
+            email: "bob@gmail.com",
+            password: "123456"
+        }
+
+        const user = await api.post("/api/auth/login").send(loginUser).expect(201).expect("Content-type", /application\/json/)
+
+        const newRoom = {
+            name: "Bob's Room",
+            password: "123456"
+        }
+
+        const room = await api.post("/api/room/createroom").send(newRoom).set("Authorization", `Bearer ${user.body.token}`).expect(201).expect("Content-type", /application\/json/)
+
+        const roomId = room.body.room.id
+
+        const joinRoom = {
+            roomId,
+            password: "123456"
+        }
+
+        const joinedRoom = await api.post("/api/room/join").set("Authorization", `Bearer ${user.body.token}`).send(joinRoom).expect(200).expect("Content-type", /application\/json/)
+        const token = joinedRoom.body.token
+        
         await new Promise((resolve, reject) => {
+            const timeOut = setTimeout(() => reject(new Error("Connection timed out")), 5000)
             clientSocket = new Client(`http://localhost:${port}`, {
                 auth: {token}
             })
 
             clientSocket.on("connect", () => {
+                clearTimeout(timeOut)
                 assert.strictEqual(clientSocket.connected, true)
                 resolve()
             })
 
             clientSocket.on("connect_error", (err) => {
+                clearTimeout(timeOut)
                 reject(err)
             })
+        })
+    })
+    
+    test("socket connection should fail with wrong token or if the user doesn't enter the room", async() => {
+        const loginUser = {
+            email: "bob@gmail.com",
+            password: "123456"
+        }
+
+        const user = await api.post("/api/auth/login").send(loginUser).expect(201).expect("Content-type", /application\/json/)
+
+        const token = user.body.token
+
+        await new Promise((resolve, reject) => {
+            const timeout = setTimeout(() => reject(new Error("Connection timed out")), 5000)
+
+            clientSocket = new Client(`http://localhost:${port}`, {
+                auth: {token}
             })
+
+            clientSocket.on("connect", () => {
+                clearTimeout(timeout)
+                assert.strictEqual(clientSocket.connected, true)
+                reject(new Error("Should not connect"))
+            })
+
+            clientSocket.on("connect_error", () => {
+                clearTimeout(timeout)
+                resolve()
+            })
+        })
     })
 })
