af728bca7f8b9b9972cd16f98d01f271b4b4ec5a
SujalMaharjan04 <maharjan.sujal100@gmail.com>
2026-03-07 14:24:31 +0545

    fix: check for userId and roomId

--- backend/sockets/socket.js
+++ backend/sockets/socket.js
@@ -19,8 +19,19 @@ const initializeServer = (server) => {
 
         try {
             const decoded = jwt.verify(token, config.SECRET)
+        
+            if (!decoded.roomId) {
+                return next(new Error("Authentication Error: No Room ID"))
+            }
+
+            if (!decoded.userId) {
+                return next(new Error("Authentication Error: No User Id"))
+            }
+
+
             socket.userId = decoded.userId
             socket.roomId = decoded.roomId
+
             next()
         }
         catch (error) {
