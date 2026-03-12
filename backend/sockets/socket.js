dc32bfb370fbb682c73b7327808ecc55297a952e
SujalMaharjan04 <maharjan.sujal100@gmail.com>
2026-03-12 13:11:19 +0545

    fix: made io global, new prisma upsert and updateMany operations to set the room_members and the is_activa status

--- backend/sockets/socket.js
+++ backend/sockets/socket.js
@@ -1,9 +1,12 @@
 const { Server } = require('socket.io')
 const jwt = require('jsonwebtoken')
 const config = require('../utils/config')
+const prisma = require("../src/db")
+
+let io
 
 const initializeServer = (server) => {
-    const io = new Server(server, {
+    io = new Server(server, {
         cors: {
             origin: "http://localhost:3001",
             methods: ["GET", "POST"]
@@ -40,7 +43,7 @@ const initializeServer = (server) => {
 
     })
 
-    io.on("connection", (socket) => {
+    io.on("connection", async (socket) => {
         console.log(`User ${socket.userName} has connected to room ${socket.roomId}`)
         socket.join(socket.roomId)
 
@@ -49,15 +52,56 @@ const initializeServer = (server) => {
             message: `${socket.userName} has joined the room`
         })
 
-        socket.emit("joined-room", {roomId: socket.roomId, message: "Succesfully joined"})
-    })
+        try {
+            await prisma.room_members.upsert({
+                where: {
+                    room_id_user_id: {
+                        user_id: socket.userId,
+                        room_id: socket.roomId
+                    }
+                },
+                update: {
+                    is_active: true
+                },
+                create: {
+                    room_id: socket.roomId,
+                    user_id: socket.userId,
+                    is_active: true
+                }
+            })
+        }
+        catch (err) {
+            console.log("update failed", err.message)
+        }
 
-    io.on("disconnect", () => {
-        socket.to(socket.roomId).emit("user-left", {
-            userId: socket.userId,
-            message: `${socket.userName} has left the room`
+        socket.emit("joined-room", {roomId: socket.roomId, message: "Succesfully joined"})
+
+        socket.on("disconnect", async() => {
+            socket.to(socket.roomId).emit("user-left", {
+                userId: socket.userId,
+                message: `${socket.userName} has left the room`
+            })
+
+            try {
+                await prisma.room_members.update({
+                    where: {
+                        room_id_user_id: {
+                            room_id: socket.roomId,
+                            user_id: socket.userId
+                        }
+                    },
+                    data: {
+                        is_active: false
+                    }
+                })
+            }
+            catch (err) {
+                console.log("update failed ", err.message)
+            }
         })
     })
+
+    
 
     return io;
 }
