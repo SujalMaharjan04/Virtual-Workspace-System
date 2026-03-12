49b187684993554947d43633669793072f8630fd
SujalMaharjan04 <maharjan.sujal100@gmail.com>
2026-03-12 13:11:57 +0545

    fix: new Func for joining the room and new test for is_active status after room joined and disconnect

--- backend/test/roomTest.test.js
+++ backend/test/roomTest.test.js
@@ -10,6 +10,33 @@ const http = require("http")
 
 let server, clientSocket, port
 
+const roomInfo = async () => {
+
+    const loginUser = {
+        email: "bob@gmail.com",
+        password: "123456"
+    }
+
+    const user = await api.post("/api/auth/login").send(loginUser).expect(201).expect("Content-type", /application\/json/)
+    const newRoom = {
+        name: "Bob's Room",
+        password: "123456"
+    }
+
+    const room = await api.post("/api/room/createroom").send(newRoom).set("Authorization", `Bearer ${user.body.token}`).expect(201).expect("Content-type", /application\/json/)
+
+    const roomId = room.body.room.id
+
+    const joinRoom = {
+        roomId,
+        password: "123456"
+    }
+
+    const joinedRoom = await api.post("/api/room/join").set("Authorization", `Bearer ${user.body.token}`).send(joinRoom).expect(200).expect("Content-type", /application\/json/)
+
+    return joinedRoom
+}
+
 before(async() => {
     await new Promise((resolve, reject) => {
         appServer.listen(3001, () => {
@@ -24,6 +51,7 @@ beforeEach(async() => {
         clientSocket.disconnect()
         clientSocket = null
     }
+    await prisma.room_members.deleteMany({})
     await prisma.room.deleteMany({})
     await prisma.user.deleteMany({})
 
@@ -124,28 +152,8 @@ describe("Testing Socket.io", () => {
     })
 
     test("after joining the room, socket connection is established", async() => {
-        const loginUser = {
-            email: "bob@gmail.com",
-            password: "123456"
-        }
+        const joinedRoom = await roomInfo()
 
-        const user = await api.post("/api/auth/login").send(loginUser).expect(201).expect("Content-type", /application\/json/)
-
-        const newRoom = {
-            name: "Bob's Room",
-            password: "123456"
-        }
-
-        const room = await api.post("/api/room/createroom").send(newRoom).set("Authorization", `Bearer ${user.body.token}`).expect(201).expect("Content-type", /application\/json/)
-
-        const roomId = room.body.room.id
-
-        const joinRoom = {
-            roomId,
-            password: "123456"
-        }
-
-        const joinedRoom = await api.post("/api/room/join").set("Authorization", `Bearer ${user.body.token}`).send(joinRoom).expect(200).expect("Content-type", /application\/json/)
         const token = joinedRoom.body.token
         
         await new Promise((resolve, reject) => {
@@ -192,6 +200,68 @@ describe("Testing Socket.io", () => {
 
             clientSocket.on("connect_error", () => {
                 clearTimeout(timeout)
+                resolve()
+            })
+        })
+    })
+
+    test("When the room is joined then the room_members is added to db and set to online", async() => {
+        const joinedRoom = await roomInfo()
+        const initialMembers = await helper.membersInRoom()
+
+        const token = joinedRoom.body.token
+        await new Promise((resolve, reject) => {
+            const timeout = setTimeout(() => reject(new Error("Connection timed out")), 3000)
+
+            clientSocket = new Client(`http://localhost:${port}`, {
+                auth: {token}
+            })
+
+            clientSocket.on("connect", async () => {
+                clearTimeout(timeout)
+                const newMembers = await helper.membersInRoom()
+
+                assert.strictEqual(newMembers.length, initialMembers.length + 1)
+                assert.strictEqual(newMembers[0].is_active, true)
+
+                resolve()
+            })
+
+            clientSocket.on("connect_error", (err) => {
+                clearTimeout(timeout)
+                reject(err)
+            })
+        })
+    })
+
+    test("when the user leave the room, is_active is false", async() => {
+        const joinedRoom = await roomInfo()
+        const token = joinedRoom.body.token
+        const initialMember = await helper.membersInRoom()
+
+        await new Promise((resolve, reject) => {
+            const timeout = setTimeout(() => reject(new Error("Connection timed out")), 3000)
+
+            clientSocket = new Client(`http://localhost:${port}`, {
+                auth: {token}
+            })
+
+            clientSocket.on("connect", () => {
+                console.log("Connected")
+            })
+
+            clientSocket.on("joined-room",() => {
+                clientSocket.disconnect()
+            })
+
+            clientSocket.on("connect_error", (err) => {
+                reject(err)
+            })
+
+            clientSocket.on("disconnect", async() => {
+                clearTimeout(timeout)
+                const members = await helper.membersInRoom()
+                assert.strictEqual(members[0].is_active, false)
                 resolve()
             })
         })
