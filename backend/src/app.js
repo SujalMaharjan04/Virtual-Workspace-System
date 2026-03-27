601de1cdd3675a6cfd6947509cb36e900bde894c
SujalMaharjan04 <maharjan.sujal100@gmail.com>
2026-03-27 22:42:53 +0545

    feature: Included the messageRouter in app

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,6 +4,7 @@ const {initializeServer} = require('../sockets/socket')
 const http = require('http')
 const authRouter = require('../modules/auth/authRoutes')
 const roomRouter = require('../modules/rooms/roomRoutes')
+const messageRouter = require('../modules/message/messageRoutes')
 
 const app = express()
 const server = http.createServer(app)
@@ -13,5 +14,6 @@ app.use(express.static(path.resolve('./public')))
 initializeServer(server)
 app.use('/api/auth', authRouter)
 app.use("/api/room", roomRouter)
+app.use("/api/message", messageRouter)
 
 module.exports = {app, server}
