956f9cc13b5f2f90cc4d9d951f55062b61cc9c9a
SujalMaharjan04 <maharjan.sujal100@gmail.com>
2026-03-27 22:42:32 +0545

    fix: File name changes and new routes for send messages and DM

---
+++ backend/modules/message/messageRoutes.js
@@ -0,0 +1,15 @@
+const { roomExtractor } = require('../../utils/middleware')
+const messageController = require('./messageController')
+const messageRouter = require("express").Router()
+
+
+//Route for getting all the messages in the room
+messageRouter.get("/get-message", roomExtractor, messageController.getMessage)
+
+//Route for sending messages to everyone
+messageRouter.post("/send-message", roomExtractor, messageController.sendMessage)
+
+//Route for sending DM
+messageRouter.post("/send-dm", roomExtractor, messageController.sendMessageDM)
+
+module.exports = messageRouter
