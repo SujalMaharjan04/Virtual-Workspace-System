f67a1b7af0b9267b284215c91a6e127881b47489
SujalMaharjan04 <maharjan.sujal100@gmail.com>
2026-03-28 23:45:40 +0545

    fix: applied try catch for error handling

--- backend/sockets/handlers/message.handler.js
+++ backend/sockets/handlers/message.handler.js
@@ -8,45 +8,57 @@ const registerMessageHandler = async(io, socket) => {
     const userName = socket.userName
 
     socket.on(MESSAGE_EVENTS.SEND_ALL, async(data) => {
-        const message = await messageService.sendMessage({
-            messages: data.message,
-            roomId,
-            userId
-        })
+        try {
+            const message = await messageService.sendMessage({
+                messages: data.message,
+                roomId,
+                userId
+            })
 
-        io.to(roomId).emit(MESSAGE_EVENTS.SEND_ALL, {
-            messageId: message.message_id,
-            sent_by: userName,
-            userId,
-            sent_at: message.sent_at,
-            message: message.message,
-        })
+            io.to(roomId).emit(MESSAGE_EVENTS.SEND_ALL, {
+                messageId: message.message_id,
+                sent_by: userName,
+                userId,
+                sent_at: message.sent_at,
+                message: message.message,
+            })
+        }
+        catch (err) {
+            console.log(err.message)
+            socket.emit("error", {message: err.message})
+        }
     })
 
     socket.on(MESSAGE_EVENTS.SEND_DM, async(data) => {
-        const message = await messageService.sendDM({
-            roomId,
-            userId,
-            sentToId: data.sentToId,
-            message: data.message
-        })
+        try {
+            const message = await messageService.sendDM({
+                roomId,
+                userId,
+                sentToId: data.sentToId,
+                message: data.message
+            })
 
-        const sockets = await io.in(roomId).fetchSockets()
-        const receiverSocket = sockets.find(s => s.userId === data.sentToId)
+            const sockets = await io.in(roomId).fetchSockets()
+            const receiverSocket = sockets.find(s => s.userId === data.sentToId)
 
-        const dmData = {
-            messageId: message.message_id,
-            userName,
-            userId,
-            message: message.message,
-            sent_at: message.sent_at
+            const dmData = {
+                messageId: message.message_id,
+                userName,
+                userId,
+                message: message.message,
+                sent_at: message.sent_at
+            }
+
+            if (receiverSocket) {
+                io.to(receiverSocket.id).emit(MESSAGE_EVENTS.RECEIVE_DM, dmData)
+            }
+
+            socket.emit(MESSAGE_EVENTS.RECEIVE_DM, dmData)
         }
-
-        if (receiverSocket) {
-            io.to(receiverSocket.id).emit(MESSAGE_EVENTS.RECEIVE_DM, dmData)
+        catch (err) {
+            console.log(err.message)
+            socket.emit("error", {message: err.message})
         }
-
-        socket.emit(MESSAGE_EVENTS.RECEIVE_DM, dmData)
     })
 
 }
