cc95b5ca47d6bd63a3a687a988d6132e7d2a4bd6
SujalMaharjan04 <maharjan.sujal100@gmail.com>
2026-03-28 14:24:27 +0545

    feature: Socket handler for message events

---
+++ backend/sockets/handlers/message.handler.js
@@ -0,0 +1,55 @@
+const MESSAGE_EVENTS = require('../events')
+const prisma = require('../../src/db')
+const messageService = require('../../modules/message/messageService')
+
+const registerMessageHandler = async(io, socket) => {
+    const userId = socket.userId
+    const roomId = socket.roomId
+    const userName = socket.userName
+
+    socket.on(MESSAGE_EVENTS.SEND_ALL, async(data) => {
+        const message = await messageService.sendMessage({
+            messages: data.message,
+            roomId,
+            userId
+        })
+
+        io.to(roomId).emit(MESSAGE_EVENTS.SEND_ALL, {
+            messageId: message.message_id,
+            sent_by: userName,
+            userId,
+            sent_at: message.sent_at,
+            message: message.message,
+        })
+    })
+
+    socket.on(MESSAGE_EVENTS.SEND_DM, async(data) => {
+        const message = await messageService.sendDM({
+            roomId,
+            userId,
+            sentToId: data.sentToId,
+            message: data.message
+        })
+
+        const sockets = await io.in(roomId).fetchSockets()
+        const receiverSocket = sockets.find(s => s.userId === data.sentToId)
+
+        const dmData = {
+            messageId: message.message_id,
+            userName,
+            userId,
+            message: message.message,
+            sent_at: message.sent_at
+        }
+
+        if (receiverSocket) {
+            io.to(receiverSocket.id).emit(MESSAGE_EVENTS.RECEIVE_DM, dmData)
+        }
+
+        socket.emit(MESSAGE_EVENTS.RECEIVE_DM, dmData)
+    })
+
+}
+
+
+module.exports = registerMessageHandler
