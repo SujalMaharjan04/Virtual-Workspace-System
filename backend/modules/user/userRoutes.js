4705efcb3a72c34e70269dfabb517576b3b16f35
SujalMaharjan04 <maharjan.sujal100@gmail.com>
2026-04-12 13:41:38 +0545

    fix: userExtractor middleware was not imported

--- backend/modules/user/userRoutes.js
+++ backend/modules/user/userRoutes.js
@@ -1,5 +1,6 @@
 const userController = require('./userController')
 const userRouter = require('express').Router()
+const {userExtractor} = require("../../utils/middleware")
 
 ///Route for getting public key
 userRouter.get("/:userId/publicKey", userExtractor, userController.getPublicKey)
