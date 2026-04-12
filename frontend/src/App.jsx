7f311b3e7c0062a88cc5658071550c2d0c03fa58
SujalMaharjan04 <maharjan.sujal100@gmail.com>
2026-04-12 22:34:26 +0545

    feature: Routes for different page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,17 @@
+import AuthPage from "./pages/AuthPage"
+import {Routes, Route} from "react-router-dom"
+
 
 const App = () => {
+
+
   return (
     <div>
-      Hello
+      <Routes>
+        <Route path = "/" element = {<AuthPage />} />
+
+        <Route path = "/home" element = {<h1>Hello</h1>} />
+      </Routes>
     </div>
   )
 }
