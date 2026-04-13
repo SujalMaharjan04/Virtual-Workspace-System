d213fe2997335eca004a79dc9df478bb3439b5c3
SujalMaharjan04 <maharjan.sujal100@gmail.com>
2026-04-13 11:38:37 +0545

    feature; Login service function and more detailed returned type and error handling

--- frontend/src/services/authService.js
+++ frontend/src/services/authService.js
@@ -2,13 +2,31 @@ import api from './api'
 
 const signUp = async(formData) => {
     try {
+        
         const response = await api.post("/auth/signup", formData)
-        return response
+
+        return {success: true, data: response.data}
+
+
     }
 
     catch (error) {
-        return error.message
+        const message = error.response?.data?.message || error.message
+
+        return {success: false, data: message}
     }
 }
 
-export default {signUp}
+const login = async(formData) => {
+    try {
+        const response = await api.post("/auth/login", formData)
+        return {success: true, data: response.data}
+    }
+    catch (error) {
+        const message = error.response?.data?.message || error.message
+
+        return {success:false, data: message}
+    }
+}
+
+export default {signUp, login}
