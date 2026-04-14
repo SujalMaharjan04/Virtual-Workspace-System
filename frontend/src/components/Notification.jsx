924a1e3e993dfd809d8806d22085a659d652099d
SujalMaharjan04 <maharjan.sujal100@gmail.com>
2026-04-14 16:52:20 +0545

    feature: Component for notification

---
+++ frontend/src/components/Notification.jsx
@@ -0,0 +1,32 @@
+import useNotificationStore from "../store/notificationStore"
+import { useEffect } from "react"
+
+
+const Notification = () => {
+    const message = useNotificationStore(state => state.message)
+    const type = useNotificationStore(state => state.type)
+    const clearNotification = useNotificationStore(state => state.clearNotification)
+
+    useEffect(() => {
+        let timer
+        if (message) {
+            timer = setTimeout(() => {
+                clearNotification()
+            }, 3000)
+        }
+        return () => clearTimeout(timer)
+        
+    }, [message, clearNotification])
+
+    if (!message) {
+        return null
+    }
+
+    return (
+        <div className = {`notification ${type === "success" ? "notification-success" : "notification-error"}`}>
+            <p>{message}</p>
+        </div>
+    )
+}
+
+export default Notification
