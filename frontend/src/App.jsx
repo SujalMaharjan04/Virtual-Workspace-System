7d0726077b2d867ae7856f14cdf228d6d962eae1
SujalMaharjan04 <maharjan.sujal100@gmail.com>
2026-04-14 16:53:28 +0545

    feature: Notification and ProtectedRoute in use

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,22 @@
 import AuthPage from "./pages/AuthPage"
 import {Routes, Route} from "react-router-dom"
+import ProtectedRoute from "./components/ProtectedRoute"
+import Notification from "./components/Notification"
 
 
 const App = () => {
 
-
   return (
     <div>
+      <Notification />
       <Routes>
         <Route path = "/" element = {<AuthPage />} />
-
-        <Route path = "/home" element = {<h1>Hello</h1>} />
+        
+        <Route path = "/dashboard" element = {
+          <ProtectedRoute>
+            <h1>Hello</h1>
+          </ProtectedRoute>
+          } />
       </Routes>
     </div>
   )
