49fdc176c7efc1aeb156a63b9cc637b7134501c9
SujalMaharjan04 <maharjan.sujal100@gmail.com>
2026-04-16 17:40:04 +0545

    feature: Room store in use and connection to the backend api

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,9 +1,12 @@
 import { useState } from "react"
 import Togglable from "./Togglable"
 import RoomForm from "./RoomForm"
+import roomService from '../services/room'
+import useRoomStore from "../store/roomStore"
 
 const Navbar = () => {
     const [activeModal, setActiveModal] = useState(null)
+    const [error, setErrors] = useState({})
     const [join, setJoin] = useState({
         "roomId": "",
         "password": ""
@@ -13,6 +16,10 @@ const Navbar = () => {
         "roomName": "",
         "password": ""
     })
+
+    const setRoomInfo = useRoomStore(state => state.setRoomInfo)
+    const setRoomToken = useRoomStore(state => state.setRoomToken)
+
 
     const handleJoin = (e) => {
         const {name, value } = e.target
@@ -30,9 +37,24 @@ const Navbar = () => {
         }))
     }
 
-    const handleJoinSubmit = (e) => {
+    const handleJoinSubmit = async(e) => {
         e.preventDefault()
-        console.log(join)
+        const errors = {}
+
+        if (!join.roomId) {
+            errors.roomId =  "Room Id required"
+        }
+
+        if (!join.password) {
+            errors.password = "Password Required"
+        }
+
+        setErrors(errors)
+
+        if (Object.keys(errors).length === 0) {
+            const response = await roomService.joinRoom(join)
+            console.log(response)
+        }
         
         setJoin({
             "roomId": "",
@@ -40,9 +62,25 @@ const Navbar = () => {
         })
     }
 
-    const handleNewSubmit = (e) => {
+    const handleNewSubmit = async(e) => {
         e.preventDefault()
-        console.log(newRoom)
+        const errors = {}
+
+        if (!newRoom.roomName) {
+            errors.roomName = "Room Name required"
+        }
+
+        if (!newRoom.password) {
+            errors.password = "Password required"
+        }
+
+        setErrors(errors)
+
+        if (Object.keys(errors).length === 0) {
+            const response = await roomService.createRoom(newRoom)
+            setRoomInfo(response.data.room)
+            setRoomToken(response.data.token)
+        }
 
         setNewRoom({
             "roomName": "",
@@ -58,10 +96,10 @@ const Navbar = () => {
             </div>
             <div className = " flex justify-end items-center text-[#F1F5F9] w-full gap-6">
                 <Togglable buttonClass = "roomButton" buttonLabel = "Join" isOpen = {activeModal === 'join'} onOpen = {() => setActiveModal("join")} onClose = {() => setActiveModal(null)}>
-                    <RoomForm topic = "Join A Room" name1 = "roomId" label1 = "Room Id" input1 = {join.roomId} input2 = {join.password} handleInput = {handleJoin} handleSubmit={handleJoinSubmit} />
+                    <RoomForm topic = "Join A Room" name1 = "roomId" label1 = "Room Id" input1 = {join.roomId} error1 = {error.roomId} error2 = {error.password} input2 = {join.password} handleInput = {handleJoin} handleSubmit={handleJoinSubmit} />
                 </Togglable>
                 <Togglable buttonClass = "roomButton" buttonLabel = "New +" isOpen = {activeModal === 'new'} onOpen = {() => setActiveModal("new")} onClose = {() => setActiveModal(null)}>
-                    <RoomForm topic = "Create A Room" name1 = "roomName" label1 = "Room Name" input1 = {newRoom.roomName} input2 = {newRoom.password} handleInput = {handleNew} handleSubmit = {handleNewSubmit} />
+                    <RoomForm topic = "Create A Room" name1 = "roomName" label1 = "Room Name" input1 = {newRoom.roomName} error1 = {error.roomName} error2 = {error.password} input2 = {newRoom.password} handleInput = {handleNew} handleSubmit = {handleNewSubmit} />
                 </Togglable>
             </div>
         </div>
