2f7cd1d5f63af891e9c83cd5d3dae31743b2fec4
SujalMaharjan04 <maharjan.sujal100@gmail.com>
2026-04-16 20:27:00 +0545

    feature & fix: Fetching the rooms on rendering and check for token before decoding for expiry

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,29 +1,48 @@
 import AuthPage from "./pages/AuthPage"
-import {Routes, Route, Navigate} from "react-router-dom"
+import {Routes, Route, Navigate, useNavigate} from "react-router-dom"
 import { useEffect } from "react"
 import {jwtDecode} from 'jwt-decode'
 import ProtectedRoute from "./components/ProtectedRoute"
 import Notification from "./components/Notification"
 import useAuthStore from "./store/authStore"
 import DashBoard from "./pages/Dashboard"
+import useRoomStore from "./store/roomStore"
+import roomService from './services/room'
 
 
 const App = () => {
   const token = useAuthStore(state => state.token)
   const logout = useAuthStore(state => state.logout)
+  const setRooms = useRoomStore(state => state.setRooms)
+  const navigate = useNavigate()
 
   useEffect(() => {
     try {
-      const {exp} = jwtDecode(token)
-      if (Date.now() >= exp * 1000) {
-        logout()
-        window.location.href = "/auth"
+        if (token) {
+          const {exp} = jwtDecode(token)
+        if (Date.now() >= exp * 1000) {
+          logout()
+          navigate("/auth")
+        }
       }
     }
     catch {
       logout()
     }
-  })
+
+
+    
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [token])
+
+  useEffect(() => {
+      const fetchRooms = async() => {
+      const getRooms = await roomService.getAllRooms()
+      setRooms(getRooms)
+    }
+
+    fetchRooms()
+  }, [])
   
   return (
     <div>
