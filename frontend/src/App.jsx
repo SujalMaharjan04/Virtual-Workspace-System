0855603ade352adb02b80d0dac5ff27aef3ad634
SujalMaharjan04 <maharjan.sujal100@gmail.com>
2026-04-16 17:37:42 +0545

    feature: Expiry check for token on page refresh

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,7 @@
 import AuthPage from "./pages/AuthPage"
 import {Routes, Route, Navigate} from "react-router-dom"
+import { useEffect } from "react"
+import {jwtDecode} from 'jwt-decode'
 import ProtectedRoute from "./components/ProtectedRoute"
 import Notification from "./components/Notification"
 import useAuthStore from "./store/authStore"
@@ -8,6 +10,20 @@ import DashBoard from "./pages/Dashboard"
 
 const App = () => {
   const token = useAuthStore(state => state.token)
+  const logout = useAuthStore(state => state.logout)
+
+  useEffect(() => {
+    try {
+      const {exp} = jwtDecode(token)
+      if (Date.now() >= exp * 1000) {
+        logout()
+        window.location.href = "/auth"
+      }
+    }
+    catch {
+      logout()
+    }
+  })
   
   return (
     <div>
