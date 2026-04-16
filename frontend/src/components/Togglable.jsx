848d3029e6f3bbf1df8be57eb373d3c7027a5b77
SujalMaharjan04 <maharjan.sujal100@gmail.com>
2026-04-16 14:53:26 +0545

    feature: New component to handle toggle of forms for room join and new room forms

---
+++ frontend/src/components/Togglable.jsx
@@ -0,0 +1,29 @@
+
+import {createPortal} from "react-dom"
+
+const Togglable = (props) => {
+    return (
+        <div className = "relative">
+            <div>
+                <button className = {props.buttonClass} onClick = {props.onOpen}>
+                    {props.buttonLabel}
+                </button>
+            </div>
+
+            {props.isOpen && createPortal(
+                    <div className = "fixed inset-0 bg-black/40 flex justify-center items-center z-50">
+                        <div className = "bg-[#0F1117] w-[50%] min-h-[50%] max-h-[70%] rounded-2xl p-6 shadow-lg relative  " onClick = {(e) => e.stopPropagation()}>
+                            {props.children}
+                            <button className = "absolute top-2 right-3 text-grey-600 text-xl" onClick = {props.onClose}>
+                                ✕
+                            </button>
+                        </div>
+                    </div>
+                , document.body)
+            }
+        </div>
+    )
+}
+
+
+export default Togglable
