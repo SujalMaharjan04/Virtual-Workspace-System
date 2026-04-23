a32f528b5c9252172eea9d21b055ab451d09ecd1
SujalMaharjan04 <maharjan.sujal100@gmail.com>
2026-04-23 22:37:09 +0545

    feature: New togglable modal form for the three button in Room Navbar (inbox, members, tasks)

--- frontend/src/components/RoomNavbar.jsx
+++ frontend/src/components/RoomNavbar.jsx
@@ -1,25 +1,34 @@
+import { useState } from "react"
 import useRoomStore from "../store/roomStore"
-
+import MemberList from "./MemberList"
+import Togglable from "./Togglable"
 
 const RoomNavbar = () => {
     const room = useRoomStore(state => state.room)
+    const [activeModal, setActiveModal] = useState(null)
     return (
-        <div className = "bg-[#1A1D2E] flex justify-between items-center w-full">
+        <div className = "bg-[#1A1D2E] flex justify-between items-center">
             <div className = "m-8 w-[50%]">
                 <h1 className = "font-bold text-3xl">{room.room_name}</h1>
             </div>
             
-            <div className = " flex justify-end items-center  w-full">
+            <div className = "flex justify-end items-center gap-6 w-full">
                 <div className = "w-[15%]">
-                    <button className = "submitBtn">Inbox</button>
+                    <Togglable buttonClass = "submitBtn" buttonLabel = "Inbox" isOpen = {activeModal === "Inbox"} onOpen = {() => setActiveModal("Inbox")} onClose = {() => setActiveModal(null)}>
+                        <MemberList />
+                    </Togglable>
                 </div>
 
                 <div className = "w-[15%]">
-                    <button className = "submitBtn">Members</button>
+                    <Togglable buttonClass = "submitBtn" buttonLabel = "Member" isOpen = {activeModal === "Member"} onOpen = {() => setActiveModal("Member")} onClose = {() => setActiveModal(null)}>
+                        <MemberList />
+                    </Togglable>
                 </div>
 
                 <div className = "w-[15%]">
-                    <button className = "submitBtn">Tasks</button>
+                    <Togglable buttonClass = "submitBtn" buttonLabel = "Tasks" isOpen = {activeModal === "Tasks"} onOpen = {() => setActiveModal("Tasks")} onClose = {() => setActiveModal(null)}>
+                        <MemberList />
+                    </Togglable>
                 </div>
             </div>
         </div>
