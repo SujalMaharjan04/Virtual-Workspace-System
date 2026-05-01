2bbdc954922016799dc692f2949d03cee3ce2c13
SujalMaharjan04 <maharjan.sujal100@gmail.com>
2026-05-01 22:05:55 +0545

    fix: The div didn't have the height causing a problem for setting an element to the bottom of the modal

--- frontend/src/components/Togglable.jsx
+++ frontend/src/components/Togglable.jsx
@@ -18,7 +18,7 @@ const Togglable = forwardRef((props, ref) => {
 
             {props.isOpen && createPortal(
                     <div className = "fixed inset-0 bg-black/40 flex justify-center items-center z-50">
-                        <div className = "bg-[#0F1117] w-[50%] min-h-[50%] max-h-[70%] rounded-2xl p-6 shadow-lg relative  " onClick = {(e) => e.stopPropagation()}>
+                        <div className = "bg-[#0F1117] w-[50%] min-h-[50%] h-full max-h-[70%] overflow-hidden rounded-2xl p-6 shadow-lg relative  " onClick = {(e) => e.stopPropagation()}>
                             {props.children}
                             <button className = "absolute top-2 right-3 text-grey-600 text-xl" onClick = {props.onClose}>
                                 ✕
