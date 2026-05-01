e4dab31cb6e3c700e4b3e7ee091515a5818d2f2e
SujalMaharjan04 <maharjan.sujal100@gmail.com>
2026-05-01 22:09:59 +0545

    added a comment

--- frontend/src/components/Togglable.jsx
+++ frontend/src/components/Togglable.jsx
@@ -18,7 +18,7 @@ const Togglable = forwardRef((props, ref) => {
 
             {props.isOpen && createPortal(
                     <div className = "fixed inset-0 bg-black/40 flex justify-center items-center z-50">
-                        <div className = "bg-[#0F1117] w-[50%] min-h-[50%] h-full max-h-[70%] overflow-hidden rounded-2xl p-6 shadow-lg relative  " onClick = {(e) => e.stopPropagation()}>
+                        <div className = "bg-[#0F1117] w-[50%] min-h-[50%] h-full max-h-[70%] overflow-hidden rounded-2xl p-6 shadow-lg relative  " onClick = {(e) => e.stopPropagation()}>  {/* h-full is added to give the container a height so that its child can extend to only that container when using h-screen and overflow-hidden to prevent the parent div from overflowing */}
                             {props.children}
                             <button className = "absolute top-2 right-3 text-grey-600 text-xl" onClick = {props.onClose}>
                                 ✕
