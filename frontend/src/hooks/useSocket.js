6e18a26ec67d47789ee50e73fff904c2807bfd8a
SujalMaharjan04 <maharjan.sujal100@gmail.com>
2026-05-30 00:07:02 +0545

    feature: Added roomHandler for socket connection

--- frontend/src/hooks/useSocket.js
+++ frontend/src/hooks/useSocket.js
@@ -1,19 +1,31 @@
 import { useEffect } from "react";
 import socket from "../socket";
+import registerRoomHandler from "../socket/handlers/roomHandlers";
 
 
 const useSocket = () => {
     useEffect(() => {
         const token = JSON.parse(localStorage.getItem("room-info")).state.token
-
         if (!token) return
 
         socket.auth = {token}
 
+        const handleConnect = () => {
+            registerRoomHandler()
+        }
+
+        const handleConnectError = (error) => {
+            console.log("Socket connection error:", error)
+        }
+
+        socket.on("connect", handleConnect)
+        socket.on("connect_error", handleConnectError)
+
         socket.connect()
 
         return () => {
-            socket.off()
+            socket.off("connect", handleConnect)
+            socket.off("connect_error", handleConnectError)
             socket.disconnect()
         }
     }, [])
