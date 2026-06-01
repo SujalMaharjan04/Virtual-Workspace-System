79be0e2afb8cb8bcb2d189e28c272df40dcf0fb8
SujalMaharjan04 <maharjan.sujal100@gmail.com>
2026-06-01 22:38:24 +0545

    feature: Added notification for when the user and admin joins

--- frontend/src/socket/handlers/roomHandlers.js
+++ frontend/src/socket/handlers/roomHandlers.js
@@ -1,10 +1,24 @@
-import socket from "../index"
+import { getSocket } from "../index"
 import {ROOM_EVENTS} from "../events"
+import useNotificationStore from "../../store/notificationStore"
 
 const registerRoomHandler = () => {
-    socket.on(ROOM_EVENTS.ADMIN_JOINED, (data) => {
-        console.log("Admin joined:", data)
-    })
+    const socket = getSocket()
+    const onAdminJoined = (data) => {
+        useNotificationStore.getState().setNotification(data.message, "message")
+    }
+
+    const onUserJoined = (data) => {
+        useNotificationStore.getState().setNotification(data.message, "success")
+    }
+
+    socket.on(ROOM_EVENTS.ADMIN_JOINED, onAdminJoined)
+    socket.on(ROOM_EVENTS.JOIN, onUserJoined)
+
+    return () => {
+        socket.off(ROOM_EVENTS.ADMIN_JOINED, onAdminJoined)
+        socket.off(ROOM_EVENTS.JOIN, onUserJoined)
+    }
 }
 
 export default registerRoomHandler
