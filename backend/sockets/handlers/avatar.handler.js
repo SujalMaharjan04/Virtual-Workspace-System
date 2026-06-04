f1baf1b1eb1a62ba127077b5a798f9c5d7bd9655
SujalMaharjan04 <maharjan.sujal100@gmail.com>
2026-06-04 18:16:49 +0545

    fix: Avatar Creation event was handled and setImmediate was placed for getAvatar called  to emit the positions of all avatar in the room

--- backend/sockets/handlers/avatar.handler.js
+++ backend/sockets/handlers/avatar.handler.js
@@ -1,31 +1,52 @@
 const {AVATAR_EVENTS} = require("../events")
 const avatarService = require('../../modules/avatar/avatar.service')
 
-const registerAvatarHandler = async(io, socket) => {
+const registerAvatarHandler = (io, socket) => {
     const userId = socket.userId
     const roomId = socket.roomId
     const userName = socket.userName
 
-    const avatars = await avatarService.getAvatar({roomId})
-    //Emits the avatar positions to other sockets
-    socket.emit(AVATAR_EVENTS.POSITION, avatars)
 
+
+    //On Avatar Creation
+    console.log("Emitting", AVATAR_EVENTS.CREATED)
+    socket.on(AVATAR_EVENTS.CREATED, async(data) => {
+        try {
+            const avatar = await avatarService.upsertAvatar({userId: data.userId, roomId:data.roomId, x: data.x, y: data.y, avatarId: data.avatarId})
+        }
+        catch (err){
+            socket.emit("error", {message: err.message})
+        }
+    })
 
     //On Avatar movement, update the db
     socket.on(AVATAR_EVENTS.MOVE, async(data) => {
         try {
-            const avatar = await avatarService.upsertAvatar({userId, roomId,  x:data.x, y:data.y})
+            const avatar = await avatarService.upsertAvatar({userId: data.userId, roomId:data.roomId, x: data.x, y: data.y, avatarId: data.avatarId, direction:data.direction})
 
             socket.to(roomId).emit(AVATAR_EVENTS.DISPLAY, {
                 userId, 
                 userName: userName,
                 x: avatar.x_axis,
-                y: avatar.y_axis
+                y: avatar.y_axis,
+                direction,
+                avatarId: avatar.avatar_id
             })
         }
 
         catch (err) {
             socket.emit("error", {message: err.message})
+        }
+    })
+
+    setImmediate(async () => {
+        try {
+            const avatars = await avatarService.getAvatar({userId, roomId})
+            //Emits the avatar positions to other sockets
+            socket.emit(AVATAR_EVENTS.POSITION, avatars)
+        }
+        catch (err) {
+            console.log(err)
         }
     })
 }
