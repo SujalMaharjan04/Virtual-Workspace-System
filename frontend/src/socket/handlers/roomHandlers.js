d4aa1a84ed4516f060cce226c322809b89815ebd
SujalMaharjan04 <maharjan.sujal100@gmail.com>
2026-06-05 22:22:31 +0545

    feature: added event listerner for user left

--- frontend/src/socket/handlers/roomHandlers.js
+++ frontend/src/socket/handlers/roomHandlers.js
@@ -12,12 +12,18 @@ const registerRoomHandler = () => {
         useNotificationStore.getState().setNotification(data.message, "success")
     }
 
+    const onUserLeft = (data) => {
+        useNotificationStore.getState().setNotification(data.message, "success")
+    }
+
     socket.on(ROOM_EVENTS.ADMIN_JOINED, onAdminJoined)
     socket.on(ROOM_EVENTS.JOIN, onUserJoined)
+    socket.on(ROOM_EVENTS.USER_LEFT, onUserLeft)
 
     return () => {
         socket.off(ROOM_EVENTS.ADMIN_JOINED, onAdminJoined)
         socket.off(ROOM_EVENTS.JOIN, onUserJoined)
+        socket.off(ROOM_EVENTS.USER_LEFT, onUserLeft)
     }
 }
 
