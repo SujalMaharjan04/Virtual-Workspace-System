e179aff860ebf52b037edaf27079bbc30e8b02c8
SujalMaharjan04 <maharjan.sujal100@gmail.com>
2026-06-05 18:43:51 +0545

    feature: Avatar JOIN, avatar MOVE for when the avatar joins the room and the avatar moves, the events are emitted and Avatar SELF for when the server sends the avatar info back to client

--- frontend/src/socket/handlers/avatarHandlers.js
+++ frontend/src/socket/handlers/avatarHandlers.js
@@ -5,6 +5,47 @@ import useAvatarStore from "../../store/avatarStore";
 const registerAvatarHandler = () => {
     const socket = getSocket()
 
+    //socket emit to server when joining the room and selecting the avatar
+    useAvatarStore.getState().registerEmitJoined(({userId, avatarId, roomId}) => {
+        socket.emit(AVATAR_EVENTS.JOIN, {
+            userId, avatarId, roomId
+        })
+    })
+
+    //server responds with the localplayer spawn if already present else at spawnpoint
+    socket.on(AVATAR_EVENTS.SELF, async(avatar) => {
+        await useAvatarStore.getState().setLocalPlayer({
+            x: avatar.x_axis, 
+            y: avatar.y_axis,
+            direction: avatar.direction,
+            avatarId: avatar.avatar_id,
+            userId: avatar.created_by,
+            roomId: avatar.room_id,
+            userName: avatar.user.name
+        })
+    })
+
+    //socket emit to server when the avatar moves 
+    useAvatarStore.getState().registerEmitMoved(({userId, roomId, avatarId, x, y, direction}) => {
+        // console.log(`userId: ${userId}, roomId: ${roomId}, avatarId: ${avatarId}, x: ${x}, y: ${y}, direction: ${direction}`)
+        socket.emit(AVATAR_EVENTS.MOVE, {
+            userId, 
+            roomId, 
+            avatarId, 
+            x, y,
+            direction
+        })
+    })
+
+    //server responds with new position of the avatar after moved
+    socket.on(AVATAR_EVENTS.DISPLAY, (avatar) => {
+        useAvatarStore.getState().updateLocalPlayer({
+            x: avatar.x_axis,
+            y: avatar.y_axis,
+            direction: avatar.direction
+        })
+    })
+ 
     socket.on(AVATAR_EVENTS.POSITION, (data) => {
             data.map(d => (
                 useAvatarStore.getState().addPlayer({userId: d.created_by, userName: d.user.name, x: d.x_axis, y: d.y_axis, direction: d.direction, avatarId: d.avatar_id  })
