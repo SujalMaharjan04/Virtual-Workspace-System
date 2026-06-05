262c14a60093204a799fe9deaf50a7ae0a543e46
SujalMaharjan04 <maharjan.sujal100@gmail.com>
2026-06-05 18:40:31 +0545

    fix: Using Join to get avatar info if present else set the db and when the avatar moves, the db is updated

--- backend/sockets/handlers/avatar.handler.js
+++ backend/sockets/handlers/avatar.handler.js
@@ -1,5 +1,6 @@
 const {AVATAR_EVENTS} = require("../events")
 const avatarService = require('../../modules/avatar/avatar.service')
+const getSpawnPoint = require("../../utils/getSpawnPoint")
 
 const registerAvatarHandler = (io, socket) => {
     const userId = socket.userId
@@ -8,33 +9,51 @@ const registerAvatarHandler = (io, socket) => {
 
 
 
-    //On Avatar Creation
-    console.log("Emitting", AVATAR_EVENTS.CREATED)
-    socket.on(AVATAR_EVENTS.CREATED, async(data) => {
-        try {
-            const avatar = await avatarService.upsertAvatar({userId: data.userId, roomId:data.roomId, x: data.x, y: data.y, avatarId: data.avatarId})
+    // //On Avatar Creation
+    // console.log("Emitting", AVATAR_EVENTS.CREATED)
+    // socket.on(AVATAR_EVENTS.CREATED, async(data) => {
+    //     try {
+    //         const avatar = await avatarService.upsertAvatar({userId: data.userId, roomId:data.roomId, x: data.x, y: data.y, avatarId: data.avatarId})
+    //     }
+    //     catch (err){
+    //         socket.emit("error", {message: err.message})
+    //     }
+    // })
+
+    //When the User joins and creates the avatar
+    socket.on(AVATAR_EVENTS.JOIN, async(data) => {
+        let avatar = await avatarService.getSelfAvatar({userId: data.userId, roomId: data.roomId})
+        if (!avatar) {
+            const spawn = await getSpawnPoint()
+            console.log(spawn)
+            avatar = await avatarService.upsertAvatar({userId: data.userId, roomId: data.roomId, x: spawn.x, y: spawn.y, avatarId: data.avatarId})
+            console.log(avatar)
         }
-        catch (err){
-            socket.emit("error", {message: err.message})
-        }
+
+        socket.emit(AVATAR_EVENTS.SELF, avatar)
     })
 
     //On Avatar movement, update the db
     socket.on(AVATAR_EVENTS.MOVE, async(data) => {
         try {
             const avatar = await avatarService.upsertAvatar({userId: data.userId, roomId:data.roomId, x: data.x, y: data.y, avatarId: data.avatarId, direction:data.direction})
-
+            if (!avatar) {
+                console.log("error")
+                return
+            }
+            console.log("EMITTING DISPLAY")
             socket.to(roomId).emit(AVATAR_EVENTS.DISPLAY, {
-                userId, 
-                userName: userName,
+                userId: avatar.created_by, 
+                userName: avatar.user?.name ?? socket.userName,
                 x: avatar.x_axis,
                 y: avatar.y_axis,
-                direction,
+                direction: avatar.direction,
                 avatarId: avatar.avatar_id
             })
         }
 
         catch (err) {
+            console.log("ERROR MOVING", err.message)
             socket.emit("error", {message: err.message})
         }
     })
