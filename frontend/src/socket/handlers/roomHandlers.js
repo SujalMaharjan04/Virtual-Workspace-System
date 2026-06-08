60c8dd1c7f59dc97972ab5582b736616e997bec5
SujalMaharjan04 <maharjan.sujal100@gmail.com>
2026-06-08 19:49:06 +0545

    feature: New event for when the user leaves the room

--- frontend/src/socket/handlers/roomHandlers.js
+++ frontend/src/socket/handlers/roomHandlers.js
@@ -1,20 +1,41 @@
 import { getSocket } from "../index"
 import {ROOM_EVENTS} from "../events"
 import useNotificationStore from "../../store/notificationStore"
+import { getActiveScene } from "../../game/scene/SceneRegistry"
+import useAvatarStore from "../../store/avatarStore"
+import useRoomStore from "../../store/roomStore"
 
 const registerRoomHandler = () => {
     const socket = getSocket()
+
+    //When the admin joins the room
     const onAdminJoined = (data) => {
         useNotificationStore.getState().setNotification(data.message, "message")
     }
 
+    //When other user joins the room
     const onUserJoined = (data) => {
         useNotificationStore.getState().setNotification(data.message, "success")
     }
 
+    //When the user leaves the room, the event is received from backend
     const onUserLeft = (data) => {
-        useNotificationStore.getState().setNotification(data.message, "success")
+        const scene = getActiveScene()
+        if (!scene) return 
+        scene.removeOtherPlayer(data.userId)
+        useAvatarStore.getState().removePlayer(data.userId)
+        useNotificationStore.getState().setNotification(`${data.userName} has left the room`, "success")
     }
+
+
+    //When the user leaves, this event is emitted
+    useRoomStore.getState().registerEmitLeave(({userId, roomId}) => {
+        socket.emit(ROOM_EVENTS.LEAVE, {
+            userId, roomId
+        })
+        useAvatarStore.getState().clearPlayer()
+        useAvatarStore.getState().removeUser()
+    }) 
 
     socket.on(ROOM_EVENTS.ADMIN_JOINED, onAdminJoined)
     socket.on(ROOM_EVENTS.JOIN, onUserJoined)
