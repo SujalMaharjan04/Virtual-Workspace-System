ffa5250e673eacdc94fca7a2460f738b6a49ed30
SujalMaharjan04 <maharjan.sujal100@gmail.com>
2026-06-16 20:51:46 +0545

    fix: Removed the unused code

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,9 +3,9 @@ const config = require('../utils/config')
 
 
 
-app.get("/", (req, res) => {
-    res.sendFile("/public/index.html")
-})
+// app.get("/", (req, res) => {
+//     res.sendFile("/public/index.html")
+// })
 
 server.listen(config.PORT, () => {
     console.log("server running on port 3001")
