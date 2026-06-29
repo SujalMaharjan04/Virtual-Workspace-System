1727ea0ba51d67087d366d6d90503237894e36cf
SujalMaharjan04 <maharjan.sujal100@gmail.com>
2026-06-29 23:08:41 +0545

    feature: New events for sharing the public keys of the user joining and encrypting the aes key by admin and sharing it with the users

--- frontend/src/socket/handlers/roomHandlers.js
+++ frontend/src/socket/handlers/roomHandlers.js
@@ -2,15 +2,29 @@ import { getSocket } from "../index"
 import {ROOM_EVENTS} from "../events"
 import useNotificationStore from "../../store/notificationStore"
 import { getActiveScene } from "../../game/scene/SceneRegistry"
+import useAuthStore from "../../store/authStore"
 import useAvatarStore from "../../store/avatarStore"
 import useRoomStore from "../../store/roomStore"
+import { decryptAESKey, encryptAESKey, generateAESkey } from "../../utils/AESkey"
 
 const registerRoomHandler = () => {
     const socket = getSocket()
 
+
     //When the admin joins the room
-    const onAdminJoined = (data) => {
+    const onAdminJoined = async(data) => {
+        const userId = useAuthStore.getState().user.id
+        const room = useRoomStore.getState().room
+        const isAdmin = userId === room.created_by
+
+
         useNotificationStore.getState().setNotification(data.message, "message")
+
+        if (isAdmin) {
+            const aesKey = await generateAESkey()
+            console.log("AES key: ", aesKey)
+            useRoomStore.getState().setAESKey(aesKey)
+        }
     }
 
     //When other user joins the room
@@ -28,6 +42,21 @@ const registerRoomHandler = () => {
         useNotificationStore.getState().setNotification(`${data.userName} has left the room`, "success")
     }
 
+    const adminEncryptsKey = async({userPublicKey, requesterSocketId}) => {
+        const aesKey = useRoomStore.getState().aesKey
+
+        const publicKeyBuffer = Uint8Array.from(atob(userPublicKey), c => c.charCodeAt(0)).buffer
+        const cryptoPublicKey = await window.crypto.subtle.importKey(
+            "spki",
+            publicKeyBuffer,
+            {name: "RSA-OAEP", hash: "SHA-256"},
+            false,
+            ["encrypt"]
+        )
+        const encryptedKey = await encryptAESKey(aesKey, cryptoPublicKey)
+        socket.emit(ROOM_EVENTS.SHARE_ENCRYPT_KEY, {encryptedKey, requesterSocketId})
+    }
+
 
     //When the user leaves, this event is emitted
     useRoomStore.getState().registerEmitLeave(({userId, roomId}) => {
@@ -38,14 +67,46 @@ const registerRoomHandler = () => {
         useAvatarStore.getState().removeUser()
     }) 
 
+    socket.on(ROOM_EVENTS.RECEIVE_AES_KEY, async(encryptedKey) => {
+        
+        let  privateKey = localStorage.getItem("privateKey")
+        
+        privateKey = Uint8Array.from(atob(privateKey), c => c.charCodeAt(0)).buffer
+        
+        privateKey = await window.crypto.subtle.importKey(
+            "pkcs8",
+            privateKey,
+            { name: "RSA-OAEP", hash: "SHA-256"},
+            false,
+            ["decrypt"]
+        )
+        
+        const key =  await decryptAESKey(encryptedKey, privateKey)
+
+        
+        useRoomStore.getState().setAESKey(key)
+    })
+
     socket.on(ROOM_EVENTS.ADMIN_JOINED, onAdminJoined)
     socket.on(ROOM_EVENTS.JOIN, onUserJoined)
+    socket.once(ROOM_EVENTS.USER_JOINED, () => {
+        const room = useRoomStore.getState().room
+        const isAdmin = room?.created_by === useAuthStore.getState().user.id
+
+        if (!isAdmin) {
+        socket.emit(ROOM_EVENTS.SHARE_PUBLIC_KEY)
+    }
+    })
+    
+    socket.on(ROOM_EVENTS.ADMIN_ENCRYPT_KEY, adminEncryptsKey)
     socket.on(ROOM_EVENTS.USER_LEFT, onUserLeft)
 
     return () => {
         socket.off(ROOM_EVENTS.ADMIN_JOINED, onAdminJoined)
         socket.off(ROOM_EVENTS.JOIN, onUserJoined)
         socket.off(ROOM_EVENTS.USER_LEFT, onUserLeft)
+        socket.off(ROOM_EVENTS.ADMIN_ENCRYPT_KEY, adminEncryptsKey)
+        socket.off(ROOM_EVENTS.RECEIVE_AES_KEY)
     }
 }
 
