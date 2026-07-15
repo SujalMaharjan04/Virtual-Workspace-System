d55a5f8a21731ec8dae1e3acf44df3735897a15d
SujalMaharjan04 <maharjan.sujal100@gmail.com>
2026-07-16 02:05:42 +0545

    fix: Refactored the code for all function to check for roles and add tasks in standard format

--- backend/modules/task/task.service.js
+++ backend/modules/task/task.service.js
@@ -9,6 +9,9 @@ const getTask = async({roomId, userId, roomRole}) => {
                 include: {
                     assignee: {select: {name: true, user_id: true}},
                     creator: {select: {name: true, user_id: true}}
+                },
+                orderBy: {
+                    created_at: "asc"
                 }
             })
         } else {
@@ -23,6 +26,9 @@ const getTask = async({roomId, userId, roomRole}) => {
                 include: {
                     assignee: {select: {name: true, user_id: true}},
                     creator: {select: {name: true, user_id: true}}
+                },
+                orderBy: {
+                    created_at: "asc"
                 }
             })
         }
@@ -34,20 +40,75 @@ const getTask = async({roomId, userId, roomRole}) => {
     }
 }
 
-const addTask = async({roomId, userId, targetUser, newTask}) => {
+const addTask = async({roomId, userId, newTask}) => {
     try {
-        const task = await prisma.task.create({
+        const user = await prisma.room_members.findUnique({
+            where: {
+                room_id_user_id: {
+                    room_id: roomId,
+                    user_id: userId
+                }
+            },
+            select: {
+                role: true
+            }
+        })
+        const userRole = user.role
+        
+        if (newTask.assignedTo && userRole !== "admin") {
+            throw new Error("Only admins can assign tasks to others")
+        }
+
+        const assignedTo = newTask.assignedTo || userId
+
+        const createdTask = await prisma.task.create({
             data: {
                 room_id: roomId,
-                assigned_to: targetUser || userId,
+                assigned_to: assignedTo,
                 created_by: userId,
-                task: newTask
+                task: newTask.title,
+                description: newTask.description,
+                priority: newTask.priority,
+                status: "PENDING",
+                deadline: new Date(newTask.deadline)
+            },
+            include: {
+                assignee: {
+                    select: {
+                        user_id: true,
+                        name: true
+                    }
+                },
+                creator: {
+                    select: {
+                        user_id: true,
+                        name: true
+                    }
+                }
             }
         })
 
-        if (!task) {
+        
+
+        if (!createdTask) {
             throw new Error("Task assignment failed")
         }
+
+        const task = {
+            ...createdTask,
+            assigned_to: {
+                id: createdTask.assignee.user_id,
+                name: createdTask.assignee.name
+            },
+            created_by: {
+                id: createdTask.creator.user_id,
+                name: createdTask.creator.name
+            }
+        }
+
+        delete task.assignee
+        delete task.creator
+
 
         return task
     }
@@ -57,26 +118,59 @@ const addTask = async({roomId, userId, targetUser, newTask}) => {
     }
 }
 
-const checkTask = async({roomId, userId, taskId}) => {
+const updateTask = async({roomId, userId, taskId, changes}) => {
     try {
+        const user = await prisma.room_members.findUnique({
+            where: {
+                room_id_user_id: {
+                    room_id: roomId,
+                    user_id: userId
+                }
+            },
+            select: {
+                role: true
+            }
+        })
+        const roomRole = user.role
         const task = await prisma.task.findUnique({
             where: {task_id: taskId}
         })
 
-        if (!task) throw new Error("Task not Found")
+        const updatingPriority = changes.priority !== undefined
+        const updatingStatus = changes.status !== undefined
+        const updatingDeadline = changes.deadline !== undefined
 
-        if (task.room_id !== roomId) throw new Error("Task doesnot belong to this room")
+
+
         
-        if (task.assigned_to !== userId) throw new Error("You can only check your own tasks")
+        if (!task) throw new Error("Task not Found")
+            
+        if (task.room_id !== roomId) throw new Error("Task doesnot belong to this room")
 
-        const checked = await prisma.task.update({
+        if ((updatingPriority || updatingDeadline) && roomRole !== "admin") {
+            throw new Error("Only admins can update priority or deadline")
+        }
+            
+        if (updatingStatus && task.assigned_to !== userId) {
+            throw new Error("Only the assignee can update task status")
+        }
+                    
+        
+        const data = {}
+
+        if (changes.priority) data.priority = changes.priority
+        if (changes.status) data.status = changes.status
+        if (changes.deadline) data.deadline = new Date(changes.deadline)
+
+
+        const updatedTask = await prisma.task.update({
             where: {
                 task_id: taskId,
             },
-            data: {completed: !task.completed}
+            data
         })
 
-        return checked
+        return updatedTask
     }
     catch (err) {
         throw err
@@ -106,4 +200,4 @@ const deleteTask = async({userId, roomRole, taskId}) => {
     }
 }
 
-module.exports = {getTask, addTask, checkTask, deleteTask}
+module.exports = {getTask, addTask, updateTask, deleteTask}
