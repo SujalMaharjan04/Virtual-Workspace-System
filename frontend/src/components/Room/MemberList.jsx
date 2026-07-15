8eb1559496125166a40ff406d4e818a2219667d1
SujalMaharjan04 <maharjan.sujal100@gmail.com>
2026-07-16 02:08:46 +0545

    feature: Task Form in use

--- frontend/src/components/Room/MemberList.jsx
+++ frontend/src/components/Room/MemberList.jsx
@@ -1,16 +1,26 @@
+import { useRef, useState } from "react"
 import useRoomStore from "../../store/roomStore"
+import Togglable from "../Shared/Togglable"
+import TaskForm from "../Tasks/TaskForm"
 
-
-const MemberList = () => {
+const MemberList = (props) => {
     const roomMembers = useRoomStore(state => state.roomMembers)
-    
+    const [activeModal, setActiveModal] = useState(null)
+    const assignToggleRef = useRef()
     return (
         <div className = "flex flex-col h-full">
             <div className = "flex flex-col items-center gap-4 flex-1 overflow-y-auto py-6 w-full">
                 {roomMembers.map(member => (
-                    <div className = "flex justify-around items-center gap-4 w-[50%]">
-                        <span className = "flex justify-center items-center gap-2"><p className = {`${member.is_active ? "bg-green-500" : "bg-red-500"} rounded-full w-3 h-3`}></p>{member.user.name}</span>
-                        <button className = "w-[20%] h-12 rounded-2xl hover:cursor-pointer hover:bg-red-500 bg-red-700">Kick</button>
+                    <div key = {member.id} className = "grid grid-cols-[1fr_auto] items-center gap-4 w-[50%]">
+                        <span className = "flex items-center gap-2"><p className = {`${member.is_active ? "bg-green-500" : "bg-red-500"} rounded-full w-3 h-3`}></p>{member.name}</span>
+                        {props.button === "Kick"
+                            ? <button className = "px-4 h-12 rounded-2xl hover:cursor-pointer hover:bg-red-500 bg-red-700">{props.button}</button> 
+                            : <Togglable ref = {assignToggleRef} wrapperClass = "flex justify-center " buttonClass = "submitBtn w-full" buttonLabel = "Assign   Task" isOpen = {activeModal === 'assign'} onOpen = {() => setActiveModal("assign")} onClose = {() => setActiveModal(null)}>
+                                <TaskForm  submitType = "Assign Task" />
+                            </Togglable>
+                        }
+                       
+                        
                     </div>
                 ))}
             </div>
