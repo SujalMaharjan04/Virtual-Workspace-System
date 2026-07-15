e26f3a43698d0f292d0cc185eb0e23da20954126
SujalMaharjan04 <maharjan.sujal100@gmail.com>
2026-07-16 02:10:08 +0545

    feature: Store for tasks

---
+++ frontend/src/store/taskStore.js
@@ -0,0 +1,46 @@
+import { create } from "zustand";
+
+const useTaskStore = create(
+    (set) => ({
+        tasks: [],
+            // [
+            //     {
+            //         id,
+            //         title: task,
+            //         description: description,
+            //         priority,
+            //         status,
+            //         assigned_by: {
+            //             name,
+            //             id,
+            //         },
+            //         assigned_to: {
+            //             name,
+            //             id
+            //         },
+            //         deadline,
+            //         created_at,
+            //         updated_at,
+            //     }
+            // ]
+        setTask: (tasks) => set({tasks}),
+
+        addTask: (task) => set((state) => ({
+            tasks: [...state.tasks, task]
+        })),
+
+
+        updateTask: (updatedTask) => set((state) => ({
+            tasks: state.tasks.map((task) => 
+            task.id === updatedTask.id ? updatedTask : task)
+        })),
+
+        removeTask: (id) => {
+            set((state) => ({
+                tasks: state.tasks.filter((task) => task.id !== id)
+            }) )
+        }
+    })
+)
+
+export default useTaskStore
