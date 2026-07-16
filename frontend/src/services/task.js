f3e5a4f4a27979c9fb9d1a9274c385bfa0fc464e
SujalMaharjan04 <maharjan.sujal100@gmail.com>
2026-07-16 15:25:02 +0545

    feature: service module for task update and delete http req

--- frontend/src/services/task.js
+++ frontend/src/services/task.js
@@ -9,4 +9,22 @@ const addTask = async(task) => {
     }
 }
 
-export default {addTask}
+const updateTask = async(newTask) => {
+    try {
+        const response = await api.put("/task/update-task", newTask)
+        return response.data
+    } catch (err) {
+        console.log("Error updating tasks", err)
+    }
+}
+
+const deleteTask = async(id) => {
+    try {
+        const response = await api.delete(`/task/remove-task/${id}`) 
+        return response.data
+    } catch (err) {
+        console.log("Error deleting tasks", err)
+    }
+}
+
+export default {addTask, updateTask, deleteTask}
