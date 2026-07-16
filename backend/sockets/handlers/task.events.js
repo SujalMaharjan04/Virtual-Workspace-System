22c914ba3674d6c5162a5699eeb361aad9c9ec0a
SujalMaharjan04 <maharjan.sujal100@gmail.com>
2026-07-16 15:23:09 +0545

    feature: Complete task creation, update and deletion events

--- backend/sockets/handlers/task.events.js
+++ backend/sockets/handlers/task.events.js
@@ -3,11 +3,40 @@ const {TASK_EVENTS} = require("../events")
 
 module.exports = (io) => {
     eventBus.on("task:added", async({roomId, task}) => {
-        const sockets = await io.in(roomId).fetchSockets()
-        const receiverSocket = sockets.find(s => s.userId === task.assigned_to.id)
+        io.to(roomId).emit(TASK_EVENTS.TASK_CREATED, task)
 
-        if (receiverSocket) {
-            io.to(receiverSocket.id).emit(TASK_EVENTS.TASK_CREATED, task)
+        if (task.assigned_to?.id) {
+            const sockets = await io.in(roomId).fetchSockets()
+            const receiverSocket = sockets.find(s => s.userId === task.assigned_to.id)
+    
+            if (receiverSocket) {
+                io.to(receiverSocket.id).emit(TASK_EVENTS.TASK_ASSIGNED, {
+                    taskId: task.id,
+                    title: task.task,
+                    roomId
+                })
+            }
         }
+    })
+
+    eventBus.on("task:updated", async({roomId, updated}) => {
+        io.to(roomId).emit(TASK_EVENTS.TASK_UPDATED, updated)
+
+        if (updated.assigned_to?.id) {
+            const sockets = await io.in(roomId).fetchSockets()
+            const receiverSocket = sockets.find(s => s.userId === updated.assigned_to.id)
+
+            if (receiverSocket) {
+                io.to(receiverSocket.id).emit(TASK_EVENTS.TASK_UPDATED_NOTIFY, {
+                    taskId: updated.id,
+                    title: updated.task,
+                    roomId
+                })
+            }
+        }
+    })
+
+    eventBus.on("task:removed", async({roomId, taskId}) => {
+        io.to(roomId).emit(TASK_EVENTS.TASK_DELETED, taskId)
     })
 }
