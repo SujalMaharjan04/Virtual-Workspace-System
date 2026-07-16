69f5d8f9b1eb54d69276e80de38fe2cf42e1e101
SujalMaharjan04 <maharjan.sujal100@gmail.com>
2026-07-16 15:21:59 +0545

    fix: removed taskPermission and bug fixes regarding / missing

--- backend/modules/task/taskRoutes.js
+++ backend/modules/task/taskRoutes.js
@@ -6,12 +6,12 @@ const {userExtractor, roomExtractor, taskPermission} = require('../../utils/midd
 taskRouter.get("/all-task", userExtractor, roomExtractor, taskController.getTask)
 
 //Route for adding a task
-taskRouter.post("/add-task", userExtractor, roomExtractor, taskPermission, taskController.addTask)
+taskRouter.post("/add-task", userExtractor, roomExtractor,  taskController.addTask)
 
 //Route for signing the task complete
-taskRouter.put("/udpate-task", userExtractor, roomExtractor, taskController.checkTask)
+taskRouter.put("/update-task", userExtractor, roomExtractor, taskController.updateTask)
 
 //Route for deleting a task
-taskRouter.delete("/remove-task", userExtractor, roomExtractor, taskPermission, taskController.deleteTask)
+taskRouter.delete("/remove-task/:taskId", userExtractor, roomExtractor, taskController.deleteTask)
 
 module.exports = taskRouter
