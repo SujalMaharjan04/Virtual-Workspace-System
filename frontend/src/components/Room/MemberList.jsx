402a15fa7daa434b9c7bcaf92d31cf4ba0e0d4cb
SujalMaharjan04 <maharjan.sujal100@gmail.com>
2026-07-16 15:23:46 +0545

    feature: Conditional rendering of kick and assign button based on whether they are admin or user themselves or not

--- frontend/src/components/Room/MemberList.jsx
+++ frontend/src/components/Room/MemberList.jsx
@@ -2,27 +2,39 @@ import { useRef, useState } from "react"
 import useRoomStore from "../../store/roomStore"
 import Togglable from "../Shared/Togglable"
 import TaskForm from "../Tasks/TaskForm"
+import useAuthStore from "../../store/authStore"
 
 const MemberList = (props) => {
     const roomMembers = useRoomStore(state => state.roomMembers)
     const [activeModal, setActiveModal] = useState(null)
+    const currentUserId = useAuthStore(state => state.user.id)
+    const currentUser = roomMembers.find(member => member.id === currentUserId)
+    const isViewerAdmin = currentUser?.role === "admin"
     const assignToggleRef = useRef()
     return (
         <div className = "flex flex-col h-full">
             <div className = "flex flex-col items-center gap-4 flex-1 overflow-y-auto py-6 w-full">
-                {roomMembers.map(member => (
+                {roomMembers.map(member => {
+                    const canKick = isViewerAdmin && member.id !== currentUserId
+                    const canAssign = isViewerAdmin && member.id !== currentUserId
+                    return(
                     <div key = {member.id} className = "grid grid-cols-[1fr_auto] items-center gap-4 w-[50%]">
                         <span className = "flex items-center gap-2"><p className = {`${member.is_active ? "bg-green-500" : "bg-red-500"} rounded-full w-3 h-3`}></p>{member.name}</span>
                         {props.button === "Kick"
-                            ? <button className = "px-4 h-12 rounded-2xl hover:cursor-pointer hover:bg-red-500 bg-red-700">{props.button}</button> 
-                            : <Togglable ref = {assignToggleRef} wrapperClass = "flex justify-center " buttonClass = "submitBtn w-full" buttonLabel = "Assign   Task" isOpen = {activeModal === 'assign'} onOpen = {() => setActiveModal("assign")} onClose = {() => setActiveModal(null)}>
-                                <TaskForm  submitType = "Assign Task" />
-                            </Togglable>
+                            ?   <>
+                                    {canKick && <button className = "px-4 h-12 rounded-2xl hover:cursor-pointer hover:bg-red-500 bg-red-700">{props.button}</button>}
+                                    
+                                </> 
+                            : <>
+                                {canAssign && <Togglable ref = {assignToggleRef} wrapperClass = "flex justify-center " buttonClass = "submitBtn w-full" buttonLabel = "Assign   Task" isOpen = {activeModal === member.id} onOpen = {() => setActiveModal(member.id)} onClose = {() => setActiveModal(null)}>
+                                    <TaskForm  submitType = "Assign Task" assignedTo = {member.id} />
+                                </Togglable>}
+                                </>
                         }
                        
                         
                     </div>
-                ))}
+                )})}
             </div>
 
             <div className = "flex justify-between items-center gap-8 w-[50%] mx-auto py-6 mt-auto">
