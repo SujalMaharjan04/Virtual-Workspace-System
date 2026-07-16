373045f0f2617cd1ad9c55ad2f7d7da70159c5c3
SujalMaharjan04 <maharjan.sujal100@gmail.com>
2026-07-16 15:25:55 +0545

    feature: Full task event handling for assigning, updating and deleting as well notify for deletion and assignment

--- frontend/src/socket/handlers/taskHandlers.js
+++ frontend/src/socket/handlers/taskHandlers.js
@@ -6,17 +6,63 @@ import { getSocket } from "../index";
 
 const registerTaskHandler = () => {
     const socket = getSocket()
-    
-    socket.on(TASK_EVENTS.TASK_CREATED, (tasks) => {
+
+    const onCreated = (tasks) => {
         const addTask = useTaskStore.getState().addTask
 
         addTask(tasks)
         useNotificationStore.getState().setNotification("Task added Successfully", "success")
         setTimeout(() => {
             useNotificationStore.getState().clearNotification()
-        }, 3000)
+        }, 5000)    
+    }
 
-    })
+    const onAssigned = (task) => {
+        useNotificationStore.getState().setNotification(`New Task Assigned: ${task.title}`, "success")
+        setTimeout(() => {
+            useNotificationStore.getState().clearNotification()
+        }, 5000)
+    }
+
+    const onUpdated = (updated) => {
+        const updateTask = useTaskStore.getState().updateTask
+        updateTask(updated)
+    }
+
+    const onUpdatedNotify = (data) => {
+        console.log(data)
+        useNotificationStore.getState().setNotification(`Task Updated`, "success")
+        setTimeout(() => useNotificationStore.getState().clearNotification(), 5000)
+    }
+    
+    const onDelete = (taskId) => {
+        useTaskStore.getState().removeTask(taskId)
+        useNotificationStore.getState().setNotification(`Task Deleted: ${taskId}`)
+        setTimeout(() => useNotificationStore.getState().clearNotification(), 5000)
+    }
+
+    socket.on(TASK_EVENTS.TASK_CREATED, onCreated)
+
+    socket.on(TASK_EVENTS.TASK_ASSIGNED, onAssigned)
+
+    socket.on(TASK_EVENTS.TASK_UPDATED, onUpdated)
+
+    socket.on(TASK_EVENTS.TASK_UPDATED_NOTIFY, onUpdatedNotify)
+
+    socket.on(TASK_EVENTS.TASK_DELETED, onDelete)
+
+    return () => {
+            
+        socket.off(TASK_EVENTS.TASK_CREATED, onCreated)
+
+        socket.off(TASK_EVENTS.TASK_ASSIGNED, onAssigned)
+
+        socket.off(TASK_EVENTS.TASK_UPDATED, onUpdated)
+
+        socket.off(TASK_EVENTS.TASK_UPDATED_NOTIFY, onUpdatedNotify)
+        
+        socket.off(TASK_EVENTS.TASK_DELETED, onDelete)
+    }
 }
 
 export default registerTaskHandler
