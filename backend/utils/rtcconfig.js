2530db9ace5322aaf27e991dfc08417e9ec3d2eb
SujalMaharjan04 <maharjan.sujal100@gmail.com>
2026-07-19 13:22:29 +0545

    fix; Typo error

--- backend/utils/rtcconfig.js
+++ backend/utils/rtcconfig.js
@@ -3,11 +3,11 @@ const getRTCConfig = () => {
         iceServers: [
             //STUN servers free from google
             {urls: "stun:stun.l.google.com:19302"},
-            {urls: "sutn:stun1.l.google.com:19302"},
+            {urls: "stun:stun1.l.google.com:19302"},
 
             //TURN server if STUN don't work
             {
-                urls: "urn:numb.viagenie.ca",
+                urls: "turn:numb.viagenie.ca",
                 username: "webrtc@live.com",
                 credential: "muazkh"
             }
